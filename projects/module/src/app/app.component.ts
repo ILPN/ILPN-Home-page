c74eb7ac2f91958750d0d1f8652acf7e39c14495
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-05-10 15:07:37 +0200

    descriptive link component

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import {Component} from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    providers: [
+        {provide: APP_BASE_HREF, useValue: ''}
+    ]
 })
 export class AppComponent {
 }
