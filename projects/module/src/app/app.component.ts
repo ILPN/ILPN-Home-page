b20ee20bb98f046708c78f19f211be4a7c737f72
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-08-12 16:37:00 +0200

    disabled info cards

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {APP_BASE_HREF} from '@angular/common';
-import {FD_PETRI_NET} from 'ilpn-components';
+import {FD_PARTIAL_ORDER, FD_PETRI_NET, FD_BPMN, FD_TRANSITION_SYSTEM, FD_LOG} from 'ilpn-components';
 
 @Component({
     selector: 'app-root',
@@ -13,5 +13,9 @@ import {FD_PETRI_NET} from 'ilpn-components';
 export class AppComponent {
 
     readonly FD_PN = FD_PETRI_NET;
+    readonly FD_BPMN = FD_BPMN;
+    readonly FD_PO = FD_PARTIAL_ORDER;
+    readonly FD_TS = FD_TRANSITION_SYSTEM;
+    readonly FD_LOG = FD_LOG;
 
 }
